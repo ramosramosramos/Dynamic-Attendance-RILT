a16947a130443610a3b1c12dfd73e297b29f3375
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-02-22 15:49:07 +0800

    users's index done

--- resources/js/Components/Cards/UserCard.jsx
+++ resources/js/Components/Cards/UserCard.jsx
@@ -1,14 +1,39 @@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import EmailIcon from '@mui/icons-material/Email';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import Dropdown from '../DropDowns/Dropdown';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 export default function UserCard({ user }) {
 
     return (
-        <div className="p-6 bg-gray-900 text-white rounded-lg shadow-2xl space-y-4">
-            <span className='flex gap-2'> <AccountCircleIcon /> <p>{user.name}</p></span>
-            <span className='flex gap-2'> <EmailIcon /> <p>{user.email}</p></span>
-            <span className='flex gap-2'> <QuestionMarkIcon /> <p>{user.role_name}</p></span>
-        </div>
+        <>
+
+            <div className='bg-gray-900 rounded-md p-2'>
+                <div className='flex justify-end'>
+                    <Dropdown>
+                        <Dropdown.Trigger>
+                            <spa className='cursor-pointer text-white'>
+                                <MoreVertIcon />
+                            </spa>
+                        </Dropdown.Trigger>
+                        <Dropdown.Content>
+                            <Dropdown.Link>
+                                Edit
+                            </Dropdown.Link>
+                            <Dropdown.Link>
+                                Delete
+                            </Dropdown.Link>
+                        </Dropdown.Content>
+                    </Dropdown>
+                </div>
+                <div className="  text-white rounded-lg shadow-2xl space-y-4">
+                    <span className='flex gap-2'> <AccountCircleIcon /> <p>{user.name}</p></span>
+                    <span className='flex gap-2 text-sm'> <EmailIcon /> <p>{user.email}</p></span>
+                    <span className='flex gap-2 text-sm'> <QuestionMarkIcon /> <p>{user.role_name}</p></span>
+                </div>
+            </div>
+
+        </>
 
     );
 }
--- resources/js/Components/DropDowns/Dropdown.jsx
+++ resources/js/Components/DropDowns/Dropdown.jsx
@@ -88,7 +88,7 @@ const Content = ({
 
 const DropdownLink = ({ className = '', children, ...props }) => {
     return (
-        <Link
+        <Link as="button" preserveScroll
             {...props}
             className={
                 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none ' +
